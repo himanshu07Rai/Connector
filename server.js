6126bfad5f54c4f154f11265e58ff2d6667ab452
himanshu07Rai <raihimanshu22345@gmail.com>
2020-12-03 14:46:51 +0530

    commented

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/',(req,res)=>{
     res.send("API running");
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;  // in heroku || in local 
 
 app.listen(PORT,console.log(`server started on port ${PORT}`))
