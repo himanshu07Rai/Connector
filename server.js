ea3f2315dfbb6cff90f269f411129cbf3e76ec58
himanshu07Rai <raihimanshu22345@gmail.com>
2020-12-03 15:00:46 +0530

    Routes created

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/',(req,res)=>{
     res.send("API running");
 })
 
+// Define routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+
 const PORT = process.env.PORT || 5000;  // in heroku || in local 
 
 app.listen(PORT,console.log(`server started on port ${PORT}`))
