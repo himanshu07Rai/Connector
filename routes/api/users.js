407b0789051d32002e19884b8fa8769ed93aadd7
himanshu07Rai <raihimanshu22345@gmail.com>
2020-12-11 14:15:46 +0530

    Profile all set

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,14 +13,12 @@ const config = require('config');
 // @access   Public
 router.post(
   '/',
-  [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check(
       'password',
       'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 })
-  ],
+    ).isLength({ min: 6 }),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
